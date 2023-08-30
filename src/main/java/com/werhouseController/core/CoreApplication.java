24c46f2b5cc3ef8451acb0d360d2402546c3601d
sanjarbek <sanjar.qodiruli@gmail.com>
2023-08-30 12:08:43 +0500

    1

---
+++ src/main/java/com/werhouseController/core/CoreApplication.java
@@ -0,0 +1,16 @@
+package com.werhouseController.core;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties
+@SpringBootApplication
+public class CoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApplication.class, args);
+	}
+
+}
